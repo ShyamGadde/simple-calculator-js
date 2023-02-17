3c32e2967a0dc85773c2e285f9fa389f1c410998
Shyamsundar Gadde <73636812+ShyamGadde@users.noreply.github.com>
2023-02-18 00:58:13 +0530

    Add init function

---
+++ js/main.js
@@ -0,0 +1,18 @@
+let runningTotal = 0;
+let buffer = "0";
+let previousOperator;
+
+const screen = document.querySelector(".screen");
+
+function buttonClick(value) {
+  console.log(value);
+}
+
+function init() {
+  document.querySelector(".calc-buttons")
+    .addEventListener("click", function(event) {
+      buttonClick(event.target.innerText);
+    })
+}
+
+init()
